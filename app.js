4787dccaf1899537d182e11d5bb1e4f0beffc81b
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-02-14 14:52:56 +0400

    Write down Functions Chapter Task 1

---
+++ app.js
@@ -0,0 +1,18 @@
+//Functions
+//Task 1
+//In this task we have to write a JavaScript function that accepts two arguments: a string and a letter and the function will
+//count the number of occurrences of the specified letter within the string.
+//Sample arguments:'w3resource.com', 'o'
+//Expected output:2.
+
+// function numberOfOccurrence(str, letter) {
+//   let counter = 0;
+//   for (i = 0; i < str.length; i++) {
+//     if (str[i] === letter) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+// console.log(numberOfOccurrence("w3resource.com", "o"));
