69a01aa2558cdc76924b5f00a3a7336180b9847d
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-03-23 00:45:50 +0400

    Write down Conditionals/Loops chapter Task 4 solution

--- app.js
+++ app.js
@@ -434,17 +434,26 @@
 //all prime numbers within that range. Use a loop and conditional statements to check if each number is prime and output the
 //prime numbers.
 
-//First way
-function isPrime(number) {
-  if (number <= 1) {
-    return false;
-  }
-  for (i = 2; i < Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
+// function isPrime(number) {
+//   if (number <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-console.log(isPrime(8));
+// function generatePrimeNumber(start, end) {
+//   let primeNumbers = [];
+//   for (let i = start; i <= end; i++) {
+//     if (isPrime(i)) {
+//       primeNumbers.push(i);
+//     }
+//   }
+//   return primeNumbers;
+// }
+
+// console.log(generatePrimeNumber(1, 100));
