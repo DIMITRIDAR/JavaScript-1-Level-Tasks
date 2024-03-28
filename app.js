70038297bdf1351f1b8266496af0270e2a79bef3
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-03-28 12:05:13 +0400

    Write down Algorithms Task 3 second way of solution

--- app.js
+++ app.js
@@ -555,8 +555,19 @@
 
 //First way
 
-let arr = [1, 4, 10, 0, 23, 2, 6];
+// Second way - Bubble Sort Algorithm
 
-let sortedArr = arr.sort((a, b) => a - b);
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-console.log(sortedArr);
+// console.log(bubbleSort([1, 4, 10, 0, 23, 2, 6]));
