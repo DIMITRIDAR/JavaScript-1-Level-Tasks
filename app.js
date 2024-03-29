169f844e29054218bae2d132020e4f84a9a5e8ac
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-03-29 13:34:09 +0400

    Write down Algorithms Task 5 second way of solution

--- app.js
+++ app.js
@@ -642,8 +642,14 @@
 
 //First Way
 
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
+// function removeDuplicates(arr) {
+//   return [...new Set(arr)];
+// }
 
-console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7]));
+// console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7]));
+
+//Second Way
+
+// let removeDuplicates = (arr) => [...new Set(arr)];
+
+// console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7]));
